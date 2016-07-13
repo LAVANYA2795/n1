0c2cbb8a4f8bfd2f92a8e0752daf9bf53725b04c
LAVANYA2795 <mlavanya2795@gmail.com>
2016-07-13 12:12:10 +0530

    Create n1.c

---
+++ n1.c
@@ -0,0 +1,17 @@
+
+       #include<stdio.h>
+       #include<conio.h>
+       int main()
+       {
+       int a,b,c;
+       clrscr();
+       printf("Enter the value of a:\n");
+       scanf("%d",&a);
+       printf("Enter the value of b:\n");
+       scanf("%d",&b);
+       printf("Enter teh value of c:\n");
+       scanf("%d",&c);
+       c=a+b;
+       printf("Enter the value %d",c);
+       return 0;
+       }
